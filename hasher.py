b0b99f3319396dc1d1a3151a8029f80edb03ce92
Abdelkarim Mouchquelita <53612008+mchklt@users.noreply.github.com>
2021-04-04 12:47:00 +0100

    Update hasher.py

--- hasher.py
+++ hasher.py
@@ -1,24 +1,50 @@
-#MOUCHQUELITA
-import hashlib
-import base64
-print("Hasher By MOUCHQUELITA\n\n")
-txt = input("\033[1;31;1m text: ").encode('utf-8')
-txt_de = txt.decode('utf-8').encode('ascii')
-Base64 = base64.b64encode(txt_de).decode('ascii')
-Base32 = base64.b32encode(txt_de).decode('ascii')
-Base16 = base64.b16encode(txt_de).decode('ascii')
-print("\n\n")
-print("\033[1;33;40mMD5:\033[0m " + hashlib.md5(txt).hexdigest() + "\n")
-print("\033[1;33;40mblake2:\033[0m " + hashlib.blake2b(txt).hexdigest() + "\n")
-print("\033[1;33;40msha1:\033[0m " + hashlib.sha1(txt).hexdigest() + "\n")
-print("\033[1;33;40msha3_256:\033[0m " + hashlib.sha3_256(txt).hexdigest() + "\n")
-print("\033[1;33;40msha3_512:\033[0m " + hashlib.sha3_512(txt).hexdigest() + "\n")
-print("\033[1;33;40mblake2s:\033[0m " + hashlib.blake2s(txt).hexdigest() + "\n")
-print("\033[1;33;40msha384:\033[0m " + hashlib.sha256(txt).hexdigest() + "\n")
-print("\033[1;33;40msha512:\033[0m " + hashlib.sha512(txt).hexdigest() + "\n")
-print("\033[1;33;40msha224:\033[0m " + hashlib.sha224(txt).hexdigest() + "\n")
-print("\033[1;33;40msha384:\033[0m " + hashlib.sha3_384(txt).hexdigest() + "\n")
-print("\033[1;33;40msha3_224:\033[0m " + hashlib.sha3_224(txt).hexdigest() + "\n") 
-print("\033[1;33;40mBase64:\033[0m " + Base64 + "\n")
-print("\033[1;33;40mBase32:\033[0m " + Base32 + "\n")
-print("\033[1;33;40mBase16:\033[0m " + Base16 + "\n")
+try :
+
+    import hashlib
+    import base64
+    import platform
+
+except Exception as e :
+    print(e)
+    exit()
+
+if platform.system().startswith("Linux", "Termux") :
+	red , green , yellow , blue , endc = '\033[91m' , '\033[92m' ,'\033[93m' , '\033[94m' , '\033[0m'
+else :
+	red = green = yellow = blue = endc = ""
+    function
+
+print(green + '''
+    1-  MD5
+    2-  SHA1
+    3-  BLAKE2B
+    4-  SHA3_256
+    5-  SHA3_512
+    6-  BLAKE2S
+    7-  SHA256
+    8-  SHA512
+    9-  SHA3_224
+    10- SHA3_384
+    11- BASE64
+    12- BASE32
+    13- BASE16
+''' + endc)
+
+text = input("Enter Your Text: ").encode("utf-8")
+choice = input("Enter You Choice: ")
+
+vars = {1  : lambda : hashlib.md5(text).hexdigest(),
+        2  : lambda : hashlib.sha1(text).hexdigest(), 
+        3  : lambda : hashlib.blake2b(text).hexdigest(), 
+        4  : lambda : hashlib.sha3_256(text).hexdigest(), 
+        5  : lambda : hashlib.sha3_512(text).hexdigest(), 
+        6  : lambda : hashlib.blake2s(text).hexdigest(), 
+        7  : lambda : hashlib.sha256(text).hexdigest(), 
+        8  : lambda : hashlib.sha512(text).hexdigest() ,
+        9  : lambda : hashlib.sha3_224(text).hexdigest(), 
+        10 : lambda : hashlib.sha3_384(text).hexdigest(), 
+        11 : lambda : base64.b64encode(text).decode(), 
+        12 : lambda : base64.b32encode(text).decode(), 
+        13 : lambda : base64.b16encode(text).decode()}
+
+print("\n" + yellow + "[+] Your Hash is : " + endc + green + vars[int(choice)]())
